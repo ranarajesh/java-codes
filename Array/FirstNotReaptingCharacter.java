a3d61d6da05877fbe300cd8fbba7dd944670355b
Rana Rajesh <rajesh86.rana@gmail.com>
2024-02-03 12:40:50 +0530

    update firstNotRepeatingCharacter file

--- Array/FirstNotReaptingCharacter.java
+++ Array/FirstNotReaptingCharacter.java
@@ -1,21 +1,42 @@
 package Array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class FirstNotReaptingCharacter {
-    public static void main(String[] args) {
-        String s = "aaabccdeef";
+    static char firstRepeatingCharByArr(String s){
         int[] alpha = new int[26];
         for (char c:s.toCharArray()) {
             alpha[c - 'a']++;
         }
-        System.out.println(Arrays.toString(alpha));
         for(char c : s.toCharArray()){
             if(alpha[c -'a'] == 1) {
-                System.out.println(c);
-                return;
+                return c;
             }
         }
-        System.out.println('-');
+        return '-';
+    }
+
+    static char firstRepeatingCharByHashMap(String s){
+        HashMap<Character, Integer> countChar = new HashMap<Character, Integer>();
+        for(char c : s.toCharArray()){
+            if(countChar.containsKey(c)){
+                countChar.put(c, countChar.get(c) +1 );
+            }else{
+                countChar.put(c, 1);
+            }
+        }
+        for(char c : s.toCharArray()){
+            if(countChar.get(c) == 1){
+                return c;
+            }
+        }
+        return '_';
+    }
+    public static void main(String[] args) {
+        String s = "aaabccdeef";
+        System.out.println("firstRepeatingCharByArr(s) : "+firstRepeatingCharByArr(s));
+        System.out.println("firstRepeatingCharByHashMap(s) : "+firstRepeatingCharByHashMap(s));
+
     }
 }
