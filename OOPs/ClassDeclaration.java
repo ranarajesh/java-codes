9a495950bd4ee784e12316267b4139045d79b3ac
Rana Rajesh <rajesh86.rana@gmail.com>
2024-02-13 12:04:39 +0530

    Oops folder added

---
+++ OOPs/ClassDeclaration.java
@@ -0,0 +1,16 @@
+package OOPs;
+
+public class ClassDeclaration {
+    
+    static int var1; // class variable / field no need to initialized, it will return its default value.
+    public static void main(String[] args) {
+        int var2; // Local variable should be initialised 
+        System.out.println(var1);
+        try {
+            System.out.println(var2);  // It will not handle in runtime error because it give compilation error. 
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+    }
+}
